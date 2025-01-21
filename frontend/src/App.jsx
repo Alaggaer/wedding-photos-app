dd63ffe08e46d7e63774836e3a679ceec3b19acf
Alaggaer <153657445+Alaggaer@users.noreply.github.com>
2025-01-21 15:16:59 +0100

    Ajout du composant App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WeddingPhotoApp } from './components/WeddingPhotoApp';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <WeddingPhotoApp />
+    </div>
+  );
+}
+
+export default App;
